3654534c8c7a380ebfa544f15c476034139ef2a7
Mark Allen <mark.a.allen@gmail.com>
2010-02-01 18:02:11 +0000

    More website updates - basic extensibility/unit testing documented

--- RestFB/website/Example.java
+++ RestFB/website/Example.java
@@ -7,27 +7,33 @@ import com.restfb.FacebookException;
 import com.restfb.Parameter;
 
 public class Example {
-  private static final String API_KEY = "403a643asd785asdf500137c659";
-  private static final String SECRET_KEY = "c53bebas96sd6676986aae7af77d06";
+  // You need to provide your own API and Secret keys here
+  private static final String API_KEY = "";
+  private static final String SECRET_KEY = "";
 
   public static void main(String[] args) throws FacebookException {
-    String sessionKey = "3.rEsn5taasdf3aOs1A__.3600.1264611600-1241234115";
+    // You need to provide your own Session key.
+    String sessionKey = "";
+
     FacebookClient facebookClient =
         new DefaultFacebookClient(API_KEY, SECRET_KEY);
 
-    // Simple API call for a single result
+    // Last parameter specifies that this API call's result should be returned
+    // to us as a Long value.
     Long uid =
         facebookClient.execute("users.getLoggedInUser", sessionKey, Long.class);
 
     System.out.println("My UID is " + uid);
 
-    // API call with result mapped to a list of classes we've defined.
-    // Note that you can pass in an arbitrary number of parameters - here we
-    // send along the query as well as the "give me HTTPS URLs" flag
+    // FQL query which asks Facebook for your friends' names, profile picture
+    // URLs, and network affiliations
     String query =
         "SELECT name, pic_big, affiliations FROM user "
             + "WHERE uid IN (SELECT uid2 FROM friend WHERE uid1=" + uid + ")";
 
+    // Executes an API call with result mapped to a list of classes we've
+    // defined. Note that you can pass in an arbitrary number of Parameters -
+    // here we send along the query as well as the "give me HTTPS URLs" flag
     List<User> users =
         facebookClient.executeForList("fql.query", sessionKey, User.class,
           Parameter.with("query", query), Parameter.with(
@@ -40,18 +46,25 @@ public class Example {
   }
 
   public static class User {
+    // By default, assumes JSON attribute name is the same as the Java field
+    // name
     @Facebook
     String name;
 
+    // If your Java field name is different than the JSON attribute name, just
+    // specify the JSON attribute name
     @Facebook("pic_big")
     String pictureUrl;
 
+    // Java doesn't remember generic type information at runtime due to type
+    // erasure. So, for a List type mapping, you have to help out by specifying
+    // what kind of type is contained in the List (Affiliation, in this case)
     @Facebook(contains = Affiliation.class)
     List<Affiliation> affiliations;
 
     public String toString() {
-      return String.format("%s (image URL is %s, affiliations: %s)", name,
-        pictureUrl, affiliations);
+      return String.format("Name: %s\nProfile Image URL: %s\nAffiliations: %s",
+        name, pictureUrl, affiliations);
     }
   }
 
