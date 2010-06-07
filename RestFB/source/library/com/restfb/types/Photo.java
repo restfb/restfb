364da54acbfe1c2fa887d9d8bdb1269de090330b
Mark Allen <mark.a.allen@gmail.com>
2010-06-07 15:35:52 +0000

    Should have been in last commit

--- RestFB/source/library/com/restfb/types/Photo.java
+++ RestFB/source/library/com/restfb/types/Photo.java
@@ -29,7 +29,6 @@ import java.util.List;
 
 import com.restfb.Facebook;
 import com.restfb.util.DateUtils;
-import com.restfb.util.ReflectionUtils;
 
 /**
  * Represents the <a
@@ -87,30 +86,6 @@ public class Photo extends NamedFacebookType {
 
     @Facebook("created_time")
     private String createdTime;
-
-    /**
-     * @see java.lang.Object#hashCode()
-     */
-    @Override
-    public int hashCode() {
-      return ReflectionUtils.hashCode(this);
-    }
-
-    /**
-     * @see java.lang.Object#equals(java.lang.Object)
-     */
-    @Override
-    public boolean equals(Object that) {
-      return ReflectionUtils.equals(this, that);
-    }
-
-    /**
-     * @see java.lang.Object#toString()
-     */
-    @Override
-    public String toString() {
-      return ReflectionUtils.toString(this);
-    }
 
     /**
      * X coordinate (as a percentage of distance from left vs. width).
