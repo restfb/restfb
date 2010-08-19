b448ba9f541057c8e7f5576dd635682e249095f9
Mark Allen <mark.a.allen@gmail.com>
2010-08-19 17:08:58 +0000

    Added first cut at Application and Checkin types for Facebook Places support

---
+++ RestFB/source/library/com/restfb/types/Application.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2010 Mark Allen.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.restfb.types;
+
+import com.restfb.Facebook;
+
+/**
+ * Represents the <a
+ * href="http://developers.facebook.com/docs/reference/api/application"
+ * >Application Graph API type</a>.
+ * 
+ * @author <a href="http://restfb.com">Mark Allen</a>
+ * @since 1.6
+ */
+public class Application extends CategorizedFacebookType {
+  @Facebook
+  private String description;
+
+  @Facebook
+  private String link;
+
+  /**
+   * The description of the application written by the 3rd party developers.
+   * 
+   * @return The description of the application written by the 3rd party
+   *         developers.
+   */
+  public String getDescription() {
+    return description;
+  }
+
+  /**
+   * A link to application dashboard on Facebook Connections feed
+   * 
+   * @return A link to application dashboard on Facebook Connections feed.
+   */
+  public String getLink() {
+    return link;
+  }
+}
