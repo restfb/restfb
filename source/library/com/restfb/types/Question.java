fa07d2fb77dd56d03a3dfa5874251f845cd942ee
Mark Allen <maa@xmog.com>
2012-09-01 00:16:21 -0400

    Added support for Question and QuestionOption graph objects. Fixes #170 (Add support for the question type.)

---
+++ source/library/com/restfb/types/Question.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright (c) 2010-2012 Mark Allen.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.restfb.types;
+
+import static com.restfb.util.DateUtils.toDateFromLongFormat;
+import static java.util.Collections.unmodifiableList;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.restfb.Facebook;
+
+/**
+ * Represents the <a
+ * href="http://developers.facebook.com/docs/reference/api/question">Question
+ * Graph API type</a>.
+ * 
+ * @author <a href="http://restfb.com">Mark Allen</a>
+ * @since 1.6.10
+ */
+public class Question implements Serializable {
+  @Facebook
+  private String id;
+
+  @Facebook
+  private NamedFacebookType from;
+
+  @Facebook
+  private String question;
+
+  @Facebook("created_time")
+  private String createdTime;
+
+  @Facebook("updated_time")
+  private String updatedTime;
+
+  @Facebook
+  private List<QuestionOption> options = new ArrayList<QuestionOption>();
+
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * Question ID.
+   * 
+   * @return Question ID.
+   */
+  public String getId() {
+    return id;
+  }
+
+  /**
+   * User who asked the question.
+   * 
+   * @return User who asked the question.
+   */
+  public NamedFacebookType getFrom() {
+    return from;
+  }
+
+  /**
+   * Text of the question.
+   * 
+   * @return Text of the question.
+   */
+  public String getQuestion() {
+    return question;
+  }
+
+  /**
+   * Time when question was created.
+   * 
+   * @return Time when question was created.
+   */
+  public Date getCreatedTime() {
+    return toDateFromLongFormat(createdTime);
+  }
+
+  /**
+   * Time when question was last updated.
+   * 
+   * @return Time when question was last updated.
+   */
+  public Date getUpdatedTime() {
+    return toDateFromLongFormat(updatedTime);
+  }
+
+  /**
+   * The list of options available as answers to the question.
+   * 
+   * @return The list of options available as answers to the question.
+   */
+  public List<QuestionOption> getOptions() {
+    return unmodifiableList(options);
+  }
+}
---
+++ source/library/com/restfb/types/QuestionOption.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2010-2012 Mark Allen.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.restfb.types;
+
+import static com.restfb.util.DateUtils.toDateFromLongFormat;
+
+import java.util.Date;
+
+import com.restfb.Facebook;
+
+/**
+ * Represents the <a
+ * href="http://developers.facebook.com/docs/reference/api/question_option"
+ * >QuestionOption Graph API type</a>.
+ * 
+ * @author <a href="http://restfb.com">Mark Allen</a>
+ * @since 1.6.10
+ */
+public class QuestionOption extends NamedFacebookType {
+  @Facebook
+  private NamedFacebookType from;
+
+  @Facebook
+  private Integer votes;
+
+  @Facebook
+  private CategorizedFacebookType object;
+
+  @Facebook("created_time")
+  private String createdTime;
+
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * User who asked the question.
+   * 
+   * @return User who asked the question.
+   */
+  public NamedFacebookType getFrom() {
+    return from;
+  }
+
+  /**
+   * Number of votes this option has received.
+   * 
+   * @return Number of votes this option has received.
+   */
+  public Integer getVotes() {
+    return votes;
+  }
+
+  /**
+   * Optional page associated with this option.
+   * 
+   * @return Optional page associated with this option.
+   */
+  public CategorizedFacebookType getObject() {
+    return object;
+  }
+
+  /**
+   * Time when option was created.
+   * 
+   * @return Time when option was created.
+   */
+  public Date getCreatedTime() {
+    return toDateFromLongFormat(createdTime);
+  }
+}
