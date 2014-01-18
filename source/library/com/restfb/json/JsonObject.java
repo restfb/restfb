61b9ee51d0e19c3a50c6833969cf741effde367b
Mark Allen <maa@xmog.com>
2014-01-18 11:59:59 -0500

    Removed unused import

--- source/library/com/restfb/json/JsonObject.java
+++ source/library/com/restfb/json/JsonObject.java
@@ -30,7 +30,6 @@ import java.io.Writer;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
