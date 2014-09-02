53823e8c73292d73548026fb42af3a4794011857
Norbert Bartels <n.bartels@phpmonkeys.de>
2014-09-03 00:00:03 +0200

    Issue #54 - hashcode, equals and toString added to User.Picture - missing in Issue #21 commit

--- source/library/com/restfb/types/User.java
+++ source/library/com/restfb/types/User.java
@@ -486,6 +486,30 @@ public class User extends NamedFacebookType {
     public Integer getHeight() {
       return height;
     }
+    
+    /**
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+      return ReflectionUtils.hashCode(this);
+    }
+
+    /**
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object that) {
+      return ReflectionUtils.equals(this, that);
+    }
+
+    /**
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+      return ReflectionUtils.toString(this);
+    }
   }
 
   /**
