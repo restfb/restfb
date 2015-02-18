cb2e7a98fa25b5a9e1d618709ba15b5a4832d950
Damian Pawlowski <damian@falconsocial.com>
2015-02-18 14:23:15 +0100

    removed unboxing

--- source/library/com/restfb/json/JsonObject.java
+++ source/library/com/restfb/json/JsonObject.java
@@ -1191,7 +1191,7 @@ public class JsonObject implements Serializable {
       if (b == '0') {
         if (s.length() > 2 && (s.charAt(1) == 'x' || s.charAt(1) == 'X')) {
           try {
-            return Integer.valueOf(Integer.parseInt(s.substring(2), 16));
+            return Integer.parseInt(s.substring(2), 16);
           } catch (Exception e) {
             /* Ignore the error */
           }
