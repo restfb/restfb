c3c385e7b3d4c1e8a7f81023691007b29266e816
Damian Pawlowski <damian@falconsocial.com>
2015-02-18 14:06:38 +0100

    toPrimitiveJavaType(String, Class) invokes inefficient new Number(String) constructor; use Number.valueOf(String) instead

--- source/library/com/restfb/DefaultJsonMapper.java
+++ source/library/com/restfb/DefaultJsonMapper.java
@@ -558,15 +558,15 @@ public class DefaultJsonMapper implements JsonMapper {
     }
 
     if (Integer.class.equals(type) || Integer.TYPE.equals(type))
-      return (T) new Integer(json);
+      return (T) Integer.valueOf(json);
     if (Boolean.class.equals(type) || Boolean.TYPE.equals(type))
-      return (T) new Boolean(json);
+      return (T) Boolean.valueOf(json);
     if (Long.class.equals(type) || Long.TYPE.equals(type))
-      return (T) new Long(json);
+      return (T) Long.valueOf(json);
     if (Double.class.equals(type) || Double.TYPE.equals(type))
-      return (T) new Double(json);
+      return (T) Double.valueOf(json);
     if (Float.class.equals(type) || Float.TYPE.equals(type))
-      return (T) new Float(json);
+      return (T) Float.valueOf(json);
     if (BigInteger.class.equals(type))
       return (T) new BigInteger(json);
     if (BigDecimal.class.equals(type))
