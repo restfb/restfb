75cf0c53882d771f465512d80fa1210ec143bb17
Norbert Bartels <n.bartels@phpmonkeys.de>
2016-02-09 23:58:30 +0100

    Issue #377: javadoc of permission enums improved

--- source/library/com/restfb/scope/ExtendedPermissions.java
+++ source/library/com/restfb/scope/ExtendedPermissions.java
@@ -28,37 +28,45 @@ public enum ExtendedPermissions implements FacebookPermissions {
 
   /**
    * Provides access to the person's primary email address via the email property on the user object.
-   * 
+   *
+   * <p>
    * Do not spam users. Your use of email must comply with both Facebook policies and with the CAN-SPAM Act.
-   * 
+   *
+   * <p>
    * Note, even if you request the email permission it is not guaranteed you will get an email address. For example, if
    * someone signed up for Facebook with a phone number instead of an email address, the email field may be empty.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * Your app may use this permission without review from Facebook.
    */
   EMAIL("email"), //
 
   /**
    * Enables your app to read a person's notifications and mark them as read.
-   * 
+   *
+   * <p>
    * This permission does not let you send notifications to a person.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
    *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
    *
-   * @deprecated Not usable since Graph API 2.4. If you use the Graph API before 2.4 ignore the
-   *             deprecation warning
+   * @deprecated Not usable since Graph API 2.4. If you use the Graph API before 2.4 ignore the deprecation warning
    */
   @Deprecated MANAGE_NOTIFICATIONS("manage_notifications"), //
 
   /**
    * Enables your app to retrieve Page Access Tokens for the Pages and Apps that the person administrates.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it. You can grant this
    * permission on behalf of people listed within the Roles section of your App's Dashboard without review by Facebook.
    */
@@ -67,10 +75,12 @@ public enum ExtendedPermissions implements FacebookPermissions {
   /**
    * Provides the access to manage call to actions of the Pages that you manage.
    *
+   * <p>
    * <strong>Review</strong>
    *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
-   * 
+   *
    * @since Graph API 2.5
    */
   PAGES_MANAGE_CTA("pages_manage_cta"), //
@@ -78,19 +88,25 @@ public enum ExtendedPermissions implements FacebookPermissions {
   /**
    * Provides access to manage leads retrieved from Lead Ads of the Pages that you manage.
    *
+   * <p>
    * <strong>Review</strong>
    *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
+   *
+   * @since Graph API 2.3
    */
   PAGES_MANAGE_LEADS("pages_manage_leads"), //
 
   /**
    * Provides the access to show the list of the Pages that you manage.
    *
+   * <p>
    * <strong>Review</strong>
    *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
-   * 
+   *
    * @since Graph API 2.5
    */
   PAGES_SHOW_LIST("pages_show_list"), //
@@ -98,135 +114,172 @@ public enum ExtendedPermissions implements FacebookPermissions {
   /**
    * Provides access to publish Posts, Open Graph actions, achievements, scores and other activity on behalf of a person
    * using your app.
-   * 
+   *
+   * <p>
    * Because this permission lets you publish on behalf of a user please read the Platform Policies to ensure you
    * understand how to properly use this permission.
-   * 
+   *
+   * <p>
    * Your app does not need to request the publish_actions permission in order to use the Feed Dialog, the Requests
    * Dialog or the Send Dialog
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
    */
   PUBLISH_ACTIONS("publish_actions"), //
 
   /**
    * Gives your app the ability to post, comment and like as any of the Pages managed by a person using your app.
-   * 
+   *
+   * <p>
    * Publishing as an individual personal account is not possible with this permission. To post as an individual, please
    * see the <code>publish_actions</code> permission.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
-   * 
-   * since Graph API 2.3
+   *
+   * @since Graph API 2.3
    */
   PUBLISH_PAGES("publish_pages"), //
 
   /**
    * Provides read-only access to the Audience Network Insights data for Apps the person owns.
    *
+   * <p>
    * <strong>Review</strong>
    *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
+   *
+   * @since Graph API 2.4
    */
   READ_AUDIENCE_NETWORK_INSIGHTS("read_audience_network_insights"), //
 
   /**
    * Provides access to the names of custom lists a person has created to organize their friends. This is useful for
    * rendering an audience selector when someone is publishing stories to Facebook from your app.
-   * 
+   *
+   * <p>
    * This permission does not give access to a list of person's friends. If you want to access a person's friends who
    * also use your app, you should use the user_friends permission.
-   * 
+   *
+   * <p>
    * This permission will also not help you invite a person's friends to use your app.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
-   * 
-   * @deprecated in Graph API 2.3 use READ_CUSTOM_FRIENDLISTS instead. If you use the Graph API before 2.3 ignore the
-   *             deprecation warning
+   *
+   * @deprecated not usable since Graph API 2.3, use <code>READ_CUSTOM_FRIENDLISTS</code> instead. If you use the Graph
+   *             API before 2.3 ignore the deprecation warning
    */
   @Deprecated READ_FRIENDLISTS("read_friendlists"), //
 
   /**
    * Provides access to the names of custom lists a person has created to organize their friends.
-   * 
+   *
+   * <p>
    * This is useful for rendering an audience selector when someone is publishing stories to Facebook from your app.
-   * 
+   *
+   * <p>
    * This permission does not give access to a list of person's friends. If you want to access a person's friends who
    * also use your app, you should use the user_friends permission.
-   * 
+   *
+   * <p>
    * This permission will also not help you invite a person's friends to use your app.
-   * 
-   * This permission was called read_friendlists before v2.3.
-   * 
+   *
+   * <p>
+   * This permission was called <code>read_friendlists</code> before v2.3.
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
+   *
+   * @since Graph API 2.3
    */
   READ_CUSTOM_FRIENDLISTS("read_custom_friendlists"), //
 
   /**
    * Provides read-only access to the Insights data for Pages, Apps and web domains the person owns.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
    */
   READ_INSIGHTS("read_insights"), //
 
   /**
    * Provides the ability to read the messages in a person's Facebook Inbox through the inbox edge and the thread node.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
    *
-   * @deprecated Not usable since Graph API 2.4. If you use the Graph API before 2.4 ignore the
-   *             deprecation warning
+   * @deprecated Not usable since Graph API 2.4. If you use the Graph API before 2.4 ignore the deprecation warning
    */
   @Deprecated READ_MAILBOX("read_mailbox"), //
 
   /**
    * Provides the ability to read from the Page Inboxes of the Pages managed by a person. This permission is often used
    * alongside the manage_pages permission.
-   * 
+   *
+   * <p>
    * This permission does not let your app read the page owner's mailbox. It only applies to the page's mailbox.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
    */
   READ_PAGE_MAILBOXES("read_page_mailboxes"), //
 
   /**
    * Provides access to read the posts in a person's News Feed, or the posts on their Profile.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
    *
-   * @deprecated Not usable since Graph API 2.4. If you use the Graph API before 2.4 ignore the
-   *             deprecation warning
+   * <p>
+   * 
+   * @deprecated Not usable since Graph API 2.4. If you use the Graph API before 2.4 ignore the deprecation warning
    */
   @Deprecated READ_STREAM("read_stream"), //
 
   /**
    * Provides the ability to set a person's attendee status on Facebook Events (eg attending, maybe, or declined).
-   * 
+   *
+   * <p>
    * This permission does not let you invite people to an event.
-   * 
+   *
+   * <p>
    * This permission does not let you update an event's details.
-   * 
+   *
+   * <p>
    * This permission does not let you create an event. There is no way to create an event via the API as of Graph API
    * v2.0.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
    */
   RSVP_EVENT("rsvp_event");
--- source/library/com/restfb/scope/UserDataPermissions.java
+++ source/library/com/restfb/scope/UserDataPermissions.java
@@ -26,12 +26,15 @@ public enum UserDataPermissions implements FacebookPermissions {
   /**
    * Provides access to a person's personal description (the 'About Me' section on their Profile) through the bio
    * property on the User object.
-   * 
+   *
+   * <p>
    * This permission does not give access to a person's public profile data. A person's name, profile picture, locale,
    * age range and gender are included by default with the public_profile permission.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
    */
   USER_ABOUT_ME("user_about_me"), //
@@ -39,9 +42,11 @@ public enum UserDataPermissions implements FacebookPermissions {
   /**
    * Provides access to all common books actions published by any app the person has used. This includes books they've
    * read, want to read, rated or quoted.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
    */
   USER_ACTIONS_BOOKS("user_actions.books"), //
@@ -49,9 +54,11 @@ public enum UserDataPermissions implements FacebookPermissions {
   /**
    * Provides access to all common Open Graph fitness actions published by any app the person has used. This includes
    * runs, walks and bikes actions.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
    */
   USER_ACTIONS_FITNESS("user_actions.fitness"), //
@@ -59,9 +66,11 @@ public enum UserDataPermissions implements FacebookPermissions {
   /**
    * Provides access to all common Open Graph music actions published by any app the person has used. This includes
    * songs they've listened to, and playlists they've created.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
    */
   USER_ACTIONS_MUSIC("user_actions.music"), //
@@ -69,9 +78,11 @@ public enum UserDataPermissions implements FacebookPermissions {
   /**
    * Provides access to all common Open Graph news actions published by any app the person has used which publishes
    * these actions. This includes news articles they've read or news articles they've published.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
    */
   USER_ACTIONS_NEWS("user_actions.news"), //
@@ -79,9 +90,11 @@ public enum UserDataPermissions implements FacebookPermissions {
   /**
    * Provides access to all common Open Graph video actions published by any app the person has used which publishes
    * these actions. This includes videos they've watched, videos they've rated and videos they want to watch.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
    */
   USER_ACTIONS_VIDEO("user_actions.video"), //
@@ -90,39 +103,51 @@ public enum UserDataPermissions implements FacebookPermissions {
    * Provides access to a person's list of activities as listed on their Profile. This is a subset of the pages they
    * have liked, where those pages represent particular interests. This information is accessed through the activities
    * connection on the user node.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
+   *
+   * @Deprecated removed Not usable since Graph API 2.3. If you use the Graph API before 2.3 ignore the deprecation
+   *             warning
    */
-  USER_ACTIVITIES("user_activities"), //
+  @Deprecated USER_ACTIVITIES("user_activities"), //
 
   /**
    * Access the date and month of a person's birthday. This may or may not include the person's year of birth, dependent
    * upon their privacy settings and the access token being used to query this field.
-   * 
+   *
+   * <p>
    * Please note most integrations will only need age_range which comes as part of the public_profile permission.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
    */
   USER_BIRTHDAY("user_birthday"), //
 
   /**
    * Provides access to a person's education history through the education field on the User object.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
    */
   USER_EDUCATION_HISTORY("user_education_history"), //
 
   /**
    * Provides read-only access to the Events a person is hosting or has RSVP'd to.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
    */
   USER_EVENTS("user_events"), //
@@ -130,47 +155,56 @@ public enum UserDataPermissions implements FacebookPermissions {
   /**
    * Provides access the list of friends that also use your app. These friends can be found on the friends edge on the
    * user object.
-   * 
+   *
+   * <p>
    * In order for a person to show up in one person's friend list, both people must have decided to share their list of
    * friends with your app and not disabled that permission during login. Also both friends must have been asked for
    * <code>user_friends</code> during the login process.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * Your app may use this permission without review from Facebook.
    */
   USER_FRIENDS("user_friends"), //
 
   /**
    * Provides access to read a person's game activity (scores, achievements) in any game the person has played.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
    */
   USER_GAMES_ACTIVITY("user_games_activity"), //
 
   /**
    * Enables your app to read the Groups a person is a member of through the groups edge on the User object.
-   * 
+   *
+   * <p>
    * This permission does not allow you to create groups on behalf of a person. It is not possible to create groups via
    * the Graph API.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
    *
-   * @deprecated Not usable since Graph API 2.4. If you use the Graph API before 2.4 ignore the
-   *             deprecation warning
+   * @deprecated Not usable since Graph API 2.4. If you use the Graph API before 2.4 ignore the deprecation warning
    */
   @Deprecated USER_GROUPS("user_groups"), //
 
   /**
    * Provides access to a person's hometown location through the hometown field on the User object. This is set by the
    * user on the Profile.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
    */
   USER_HOMETOWN("user_hometown"), //
@@ -178,19 +212,26 @@ public enum UserDataPermissions implements FacebookPermissions {
   /**
    * Provides access to the list of interests in a person's Profile. This is a subset of the pages they have liked which
    * represent particular interests.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
+   *
+   * @Deprecated removed Not usable since Graph API 2.3. If you use the Graph API before 2.3 ignore the deprecation
+   *             warning
    */
-  USER_INTERESTS("user_interests"), //
+  @Deprecated USER_INTERESTS("user_interests"), //
 
   /**
    * Provides access to the list of all Facebook Pages and Open Graph objects that a person has liked. This list is
    * available through the likes edge on the User object.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
    */
   USER_LIKES("user_likes"), //
@@ -198,11 +239,14 @@ public enum UserDataPermissions implements FacebookPermissions {
   /**
    * Provides access to a person's current city through the location field on the User object. The current city is set
    * by a person on their Profile.
-   * 
+   *
+   * <p>
    * The current city is not necessarily the same as a person's hometown.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
    */
   USER_LOCATION("user_location"), //
@@ -210,11 +254,14 @@ public enum UserDataPermissions implements FacebookPermissions {
   /**
    * Enables your app to read the Groups a person is an admin of through the groups edge on the User object.
    *
+   * <p>
    * This permission does not allow you to create groups on behalf of a person. It is not possible to create groups via
    * the Graph API. This does not let you read the groups a user is just a member of.
    *
+   * <p>
    * <strong>Review</strong>
    *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
    */
   USER_MANAGED_GROUPS("user_managed_groups"), //
@@ -222,20 +269,25 @@ public enum UserDataPermissions implements FacebookPermissions {
   /**
    * Provides access to the photos a person has uploaded or been tagged in. This is available through the photos edge on
    * the User object.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
    */
   USER_PHOTOS("user_photos"), //
 
   /**
    * Provides access to the posts on a person's Timeline.
-   * 
+   *
+   * <p>
    * Includes their own posts, posts they are tagged in, and posts other people make on their Timeline.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
    * 
    * @since 1.10.0
@@ -244,9 +296,11 @@ public enum UserDataPermissions implements FacebookPermissions {
 
   /**
    * Provides access to a person's relationship interests as the <code>interested_in</code> field on the User object.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
    */
   USER_RELATIONSHIP_DETAILS("user_relationship_details"), //
@@ -254,59 +308,70 @@ public enum UserDataPermissions implements FacebookPermissions {
   /**
    * Provides access to a person's relationship status, significant other and family members as fields on the User
    * object.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
    */
   USER_RELATIONSHIPS("user_relationships"), //
 
   /**
    * Provides access to a person's religious and political affiliations.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
    */
   USER_RELIGION_POLITICS("user_religion_politics"), //
 
   /**
    * Provides access to a person's statuses. These are posts on Facebook which don't include links, videos or photos.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
    *
-   * @deprecated Not usable since Graph API 2.4. If you use the Graph API before 2.4 ignore the
-   *             deprecation warning. If you are calling the endpoint <code>/{user-id}/posts</code>
-   *             or <code>/{user-id}/feed</code>, ask for the <code>user_posts</code> permission
-   *             instead (only v2.3 or older).
+   * @deprecated Not usable since Graph API 2.4. If you use the Graph API before 2.4 ignore the deprecation warning. If
+   *             you are calling the endpoint <code>/{user-id}/posts</code> or <code>/{user-id}/feed</code>, ask for the
+   *             <code>user_posts</code> permission instead (only v2.3 or older).
    */
   @Deprecated USER_STATUS("user_status"), //
 
   /**
    * Provides access to the Places a person has been tagged at in photos, videos, statuses and links.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
    */
   USER_TAGGED_PLACES("user_tagged_places"), //
 
   /**
    * Provides access to the videos a person has uploaded or been tagged in.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
    */
   USER_VIDEOS("user_videos"), //
 
   /**
    * Provides access to the person's personal website URL via the website field on the User object.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
    */
   USER_WEBSITE("user_website"), //
@@ -314,9 +379,11 @@ public enum UserDataPermissions implements FacebookPermissions {
   /**
    * Provides access to a person's work history and list of employers via the <code>work</code> field on the User
    * object.
-   * 
+   *
+   * <p>
    * <strong>Review</strong>
-   * 
+   *
+   * <p>
    * If your app requests this permission Facebook will have to review how your app uses it.
    */
   USER_WORK_HISTORY("user_work_history");
