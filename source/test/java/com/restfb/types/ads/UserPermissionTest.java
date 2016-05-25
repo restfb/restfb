8a89e6845d4c80fae67dd9f383626de355834494
Norbert Bartels <n.bartels@phpmonkeys.de>
2016-05-26 00:08:00 +0200

    Issue #462 - Marketing API UserPermission type added

---
+++ source/test/java/com/restfb/types/ads/UserPermissionTest.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2010-2016 Mark Allen, Norbert Bartels.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.restfb.types.ads;
+
+import static junit.framework.TestCase.assertNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import com.restfb.AbstractJsonMapperTests;
+
+import org.junit.Test;
+
+import java.util.List;
+
+public class UserPermissionTest extends AbstractJsonMapperTests {
+
+  @Test
+  public void checkMultipleUserPermission() {
+    List<UserPermission> permissionList =
+        createJsonMapper().toJavaList(jsonFromClasspath("ads/v2_6/userpermissionslist"), UserPermission.class);
+    assertNotNull(permissionList);
+    for (UserPermission permission : permissionList) {
+      assertNotNull(permission);
+      assertNotNull(permission.getStatus());
+      if ("INVITED".equals(permission.getStatus())) {
+        assertNull(permission.getBusinessPersona());
+        assertNull(permission.getUser());
+      } else {
+        assertNotNull(permission.getBusinessPersona());
+        assertNotNull(permission.getUser());
+      }
+      assertNotNull(permission.getRole());
+
+      if (permission.getUpdatedTime() != null) {
+        assertEquals(1453390430000L, permission.getCreatedTime().getTime());
+        assertEquals(1453390430000L, permission.getUpdatedTime().getTime());
+      }
+    }
+  }
+}
