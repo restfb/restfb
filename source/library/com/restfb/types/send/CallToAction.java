29dd2aaa32b62371697c96de9b8a712578f64551
Norbert Bartels <n.bartels@phpmonkeys.de>
2016-06-10 23:35:21 +0200

    Issue #472 - messenger api welcome message container added

---
+++ source/library/com/restfb/types/send/CallToAction.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2010-2016 Mark Allen, Norbert Bartels.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.restfb.types.send;
+
+import com.restfb.Facebook;
+
+/**
+ * Used to send a new welcome message to Facebook and set it on a page. Have a look at the integration tests or the
+ * documentation for further information
+ * <p>
+ * have a look at the
+ * <a href="https://developers.facebook.com/docs/messenger-platform/send-api-reference#welcome_message_configuration">
+ * Facebook documentation</a>
+ * </p>
+ *
+ */
+public class CallToAction {
+
+  @Facebook
+  private Message message;
+
+  public CallToAction(Message message) {
+    this.message = message;
+  }
+}
