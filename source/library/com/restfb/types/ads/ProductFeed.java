490f7b8efa6a894859efd6a8ca8a572b5eed0c1a
Norbert Bartels <n.bartels@phpmonkeys.de>
2016-06-30 21:51:17 +0200

    Issue #487 - some product types added; PartnerCategory, ProductCatalog, ProductFeed and so on

---
+++ source/library/com/restfb/types/ads/ProductFeed.java
@@ -0,0 +1,101 @@
+/**
+ * Copyright (c) 2010-2016 Mark Allen, Norbert Bartels.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.restfb.types.ads;
+
+import static com.restfb.util.DateUtils.toDateFromLongFormat;
+
+import com.restfb.Facebook;
+import com.restfb.JsonMapper;
+
+import java.util.Date;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Represents the <a href="https://developers.facebook.com/docs/marketing-api/reference/product-feed/">Product Feed
+ * type</a>
+ */
+public class ProductFeed extends BaseAdsObject {
+
+  @Getter
+  @Setter
+  @Facebook
+  private String country;
+
+  @Getter
+  @Setter
+  private Date createdTime;
+
+  @Facebook("created_time")
+  private String rawCreatedTime;
+
+  @Getter
+  @Setter
+  @Facebook("default_currency")
+  private String defaultCurrency;
+
+  @Getter
+  @Setter
+  @Facebook("deletion_enabled")
+  private Boolean deletionEnabled;
+
+  @Getter
+  @Setter
+  @Facebook
+  private String delimiter;
+
+  @Getter
+  @Setter
+  @Facebook
+  private String encoding;
+
+  @Getter
+  @Setter
+  @Facebook("file_name")
+  private String fileName;
+
+  @Getter
+  @Setter
+  @Facebook("latest_upload")
+  private ProductFeedUpload latestUpload;
+
+  @Getter
+  @Setter
+  @Facebook("product_count")
+  private Long productCount;
+
+  @Getter
+  @Setter
+  @Facebook("quoted_fields_mode")
+  private String quotedFieldsMode;
+
+  @Getter
+  @Setter
+  @Facebook
+  private ProductFeedSchedule schedule;
+
+  @JsonMapper.JsonMappingCompleted
+  void convertTime() {
+    createdTime = toDateFromLongFormat(rawCreatedTime);
+  }
+}
