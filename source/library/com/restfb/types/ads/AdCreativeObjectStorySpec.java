22e561302963153c97e39d83016583fa122f428c
Norbert Bartels <n.bartels@phpmonkeys.de>
2016-07-10 16:24:42 +0200

    Issue #508 - creative_id field removed from AdCreative type, javadoc improved

--- source/library/com/restfb/types/ads/AdCreativeObjectStorySpec.java
+++ source/library/com/restfb/types/ads/AdCreativeObjectStorySpec.java
@@ -27,43 +27,105 @@ import com.restfb.types.AbstractFacebookType;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Represents the <a href="https://developers.facebook.com/docs/marketing-api/reference/ad-creative-object-story-spec/">
+ * AdCreativeObjectStorySpec</a> Marketing API type.
+ */
 public class AdCreativeObjectStorySpec extends AbstractFacebookType {
 
+  /**
+   * The Instagram user account that the story will be posted to
+   *
+   * -- GETTER --
+   *
+   * @return The Instagram user account that the story will be posted to
+   */
   @Getter
   @Setter
   @Facebook("instagram_actor_id")
   private String instagramActorId;
 
+  /**
+   * The spec for a link page post or carousel ad
+   *
+   * -- GETTER --
+   *
+   * @return The spec for a link page post or carousel ad
+   */
   @Getter
   @Setter
   @Facebook("link_data")
   private AdCreativeLinkData linkData;
 
+  /**
+   * The spec for an offer page post.
+   *
+   * -- GETTER --
+   *
+   * @return The spec for an offer page post.
+   */
   @Getter
   @Setter
   @Facebook("offer_data")
   private AdCreativeOfferData offerData;
 
+  /**
+   * ID of a Facebook page.
+   *
+   * An unpublished page post will be created on this page. User must have Admin or Editor role for this page.
+   *
+   * -- GETTER --
+   *
+   * @return ID of a Facebook page.
+   */
   @Getter
   @Setter
   @Facebook("page_id")
   private String pageId;
 
+  /**
+   * The spec for a photo page post.
+   *
+   * -- GETTER --
+   *
+   * @return The spec for a photo page post.
+   */
   @Getter
   @Setter
   @Facebook("photo_data")
   private AdCreativePhotoData photoData;
 
+  /**
+   * The spec for a template link page post as used in Dynamic Product Ads.
+   *
+   * -- GETTER --
+   *
+   * @return The spec for a template link page post as used in Dynamic Product Ads.
+   */
   @Getter
   @Setter
   @Facebook("template_data")
   private AdCreativeLinkData templateData;
 
+  /**
+   * The spec for a text page post.
+   *
+   * -- GETTER --
+   *
+   * @return The spec for a text page post.
+   */
   @Getter
   @Setter
   @Facebook("text_data")
   private AdCreativeTextData textData;
 
+  /**
+   * The spec for a video page post.
+   *
+   * -- GETTER --
+   *
+   * @return The spec for a video page post.
+   */
   @Getter
   @Setter
   @Facebook("video_data")
