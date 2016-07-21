0a3c24b84067395901797c0a7458e1f7e3a1aae1
Alex Laird <alexlaird@sparkcentral.com>
2016-07-21 11:25:33 -0700

    Adding quick replies.

--- source/test/java/com/restfb/types/SendApiTest.java
+++ source/test/java/com/restfb/types/SendApiTest.java
@@ -219,4 +219,21 @@ public class SendApiTest extends AbstractJsonMapperTests {
             "{\"attachment\":{\"payload\":{\"checkin_url\":\"http://www.google.com/checkin\",\"intro_message\":\"Intro Message\",\"template_type\":\"airline_checkin\",\"locale\":\"en_US\",\"pnr_number\":\"ABCDEF\"},\"type\":\"template\"}}",
             recipientJsonString, false);
   }
+
+  @Test
+  public void messageWithQuickRepliesAndMetadata() throws JSONException {
+    QuickReply quickReply1 = new QuickReply("text", "title 1", "payload 1");
+    QuickReply quickReply2 = new QuickReply("text", "title 2", "payload 2");
+    Message message = new Message("message text");
+    message.setMetadata("metadata payload");
+    message.addQuickReply(quickReply1);
+    message.addQuickReply(quickReply2);
+
+    DefaultJsonMapper mapper = new DefaultJsonMapper();
+    String messageJsonString = mapper.toJson(message, true);
+    System.out.println(messageJsonString);
+
+    JSONAssert.assertEquals("{\"quickReplies\":[{\"payload\":\"payload 1\",\"title\":\"title 1\",\"contentType\":\"text\"},{\"payload\":\"payload 2\",\"title\":\"title 2\",\"contentType\":\"text\"}],\"metadata\":\"metadata payload\",\"text\":\"message text\"}",
+            messageJsonString, false);
+  }
 }
