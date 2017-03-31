a4d276adf08daa0f73e4cf7e9ad7b01bbb7fc396
Norbert Bartels <n.bartels@phpmonkeys.de>
2017-03-31 22:09:09 +0200

    Issue #709: publish message to a group integration test added

---
+++ source/test/java/com/restfb/integration/PublishMessageToGroupITCase.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2010-2017 Mark Allen, Norbert Bartels.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.restfb.integration;
+
+import com.restfb.DefaultFacebookClient;
+import com.restfb.Parameter;
+import com.restfb.Version;
+import com.restfb.integration.base.NeedFacebookWriteAccess;
+import com.restfb.integration.base.RestFbIntegrationTestBase;
+import com.restfb.types.GraphResponse;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+@NeedFacebookWriteAccess
+public class PublishMessageToGroupITCase extends RestFbIntegrationTestBase {
+
+  @Test
+  public void publishMessageToGroup() {
+
+    // needed permissions: publish_actions, user_managed_groups
+    DefaultFacebookClient client =
+        new DefaultFacebookClient(getTestSettings().getUserAccessToken(), Version.VERSION_2_8);
+
+    GraphResponse result = client.publish(getTestSettings().getGroupId() + "/feed", GraphResponse.class,
+      Parameter.with("message", "This is a great test message"));
+
+    Assert.assertTrue(result.isSuccess());
+  }
+}
