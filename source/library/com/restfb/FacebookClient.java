c4d96cec7b54940b3e41edf3b71c61ced59f8ab9
ClaudioBull <cavina.claudio@gmail.com>
2017-05-25 16:06:54 +0200

    PR #732 - Update FacebookClient.java Getter for DebugTokenError added
    When I debug specific token i'm not able to get error message, I noticed that there is no get method for retrieving  DebugTokenError variable associated with DebugTokenInfo class

--- source/library/com/restfb/FacebookClient.java
+++ source/library/com/restfb/FacebookClient.java
@@ -863,6 +863,15 @@ public interface FacebookClient {
     public JsonObject getMetaData() {
       return metadata;
     }
+    
+     /**
+     * All Error data associated with access token debug.
+     * 
+     * @return debug token error
+     */
+    public DebugTokenError getDebugTokenError() {
+      return error;
+    }
 
   }
 
