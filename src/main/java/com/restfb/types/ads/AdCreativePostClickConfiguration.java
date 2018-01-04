127b1864b027a7c8af7a988ab695136b93958097
Norbert Bartels <n.bartels@phpmonkeys.de>
2018-01-04 22:13:46 +0100

    Issue #836 - copyright changed for 2018

--- src/main/java/com/restfb/types/ads/AdCreativePostClickConfiguration.java
+++ src/main/java/com/restfb/types/ads/AdCreativePostClickConfiguration.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010-2017 Mark Allen, Norbert Bartels.
+ * Copyright (c) 2010-2018 Mark Allen, Norbert Bartels.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -19,7 +19,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-
 package com.restfb.types.ads;
 
 import com.restfb.Facebook;
