eeefc8be7add82fb0af6b3c3691409c9b6793c41
Norbert Bartels <n.bartels@phpmonkeys.de>
2018-02-22 00:53:43 +0100

    Issue #855 - instagram user added

---
+++ src/main/java/com/restfb/types/instagram/IgUser.java
@@ -0,0 +1,109 @@
+/**
+ * Copyright (c) 2010-2018 Mark Allen, Norbert Bartels.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.restfb.types.instagram;
+
+import com.restfb.Facebook;
+import com.restfb.types.NamedFacebookType;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Represents the <a href="https://developers.facebook.com/docs/instagram-api/reference/user/">instagram user</a> type
+ * used in the Graph API
+ */
+public class IgUser extends NamedFacebookType {
+
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * Biography of the user
+   */
+  @Getter
+  @Setter
+  @Facebook
+  private String biography;
+
+  /**
+   * Loads business discovery information
+   */
+  @Getter
+  @Setter
+  @Facebook("business_discovery")
+  private IgUser businessDiscovery;
+
+  /**
+   * Active follower count of the user
+   */
+  @Getter
+  @Setter
+  @Facebook("followers_count")
+  private Long followersCount;
+
+  /**
+   * Active follows count of the user
+   */
+  @Getter
+  @Setter
+  @Facebook("follows_count")
+  private Long followsCount;
+
+  /**
+   * Ig Id of the user
+   */
+  @Getter
+  @Setter
+  @Facebook("ig_id")
+  private Long igId;
+
+  /**
+   * Filtered media count of the user
+   */
+  @Getter
+  @Setter
+  @Facebook("media_count")
+  private Long mediaCount;
+
+  /**
+   * The cdn url to query the raw profile picture of the user
+   */
+  @Getter
+  @Setter
+  @Facebook("profile_picture_url")
+  private String profilePictureUrl;
+
+  /**
+   * Username handle of the user
+   */
+  @Getter
+  @Setter
+  @Facebook
+  private String username;
+
+  /**
+   * Url in the profile
+   */
+  @Getter
+  @Setter
+  @Facebook
+  private String website;
+}
---
+++ src/test/java/com/restfb/types/instagram/IgUserTest.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2010-2018 Mark Allen, Norbert Bartels.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.restfb.types.instagram;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+import com.restfb.AbstractJsonMapperTests;
+
+public class IgUserTest extends AbstractJsonMapperTests {
+
+  @Test
+  public void checkUser() {
+    IgUser igUser = createJsonMapper().toJavaObject(jsonFromClasspath("instagram/user"), IgUser.class);
+    assertNull(igUser.getBiography());
+    assertEquals("tester2018", igUser.getUsername());
+    assertNull(igUser.getWebsite());
+    assertEquals(9876543L, igUser.getIgId().longValue());
+    assertEquals(10l, igUser.getFollowersCount().longValue());
+    assertEquals(15l, igUser.getFollowsCount().longValue());
+    assertEquals(2l, igUser.getMediaCount().longValue());
+    assertEquals("Tester Test", igUser.getName());
+  }
+}
