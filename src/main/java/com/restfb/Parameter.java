ad0da0ed72a47a29bb8773e93fc4e822f262512b
Pyry Koivisto <pyry.koivisto@nosto.com>
2018-08-06 14:10:28 +0300

    Add whitespace

--- src/main/java/com/restfb/Parameter.java
+++ src/main/java/com/restfb/Parameter.java
@@ -62,7 +62,7 @@ public final class Parameter {
   private Parameter(String name, Object value, JsonMapper jsonMapper) {
     if (isBlank(name) || value == null) {
       throw new IllegalArgumentException(Parameter.class + " instances must have a non-blank name and non-null value."
-              + "Got instead name:" + name + ", value:" + value);
+              + " Got instead name:" + name + ", value:" + value);
     }
 
     if (jsonMapper == null) {
