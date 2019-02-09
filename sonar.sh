82d49e2565595bae0c5a61e069e82190c69103ca
Norbert Bartels <n.bartels@phpmonkeys.de>
2019-02-09 23:02:13 +0100

    Issue #963 - license information changed to 2019

--- sonar.sh
+++ sonar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2010-2018 Mark Allen, Norbert Bartels.
+# Copyright (c) 2010-2019 Mark Allen, Norbert Bartels.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
