78a640ee638c53400f81bfb5ccbaf3010b1c2061
Norbert Bartels <n.bartels@phpmonkeys.de>
2021-09-26 16:05:04 +0200

    Issue #1173: Graph api 12 deprecated fields in user object

--- src/main/lombok/com/restfb/types/User.java
+++ src/main/lombok/com/restfb/types/User.java
@@ -52,11 +52,13 @@ public class User extends NamedFacebookType implements HasProfilePicture, HasCov
   /**
    * Social context for this person
    *
+   * @deprecated with Graph API 12 or December 13, 2021 for all versions
    * @return Social context for this person
    */
   @Getter
   @Setter
   @Facebook
+  @Deprecated
   private FacebookType context;
 
   /**
