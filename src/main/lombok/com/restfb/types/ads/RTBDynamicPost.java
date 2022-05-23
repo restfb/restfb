c64b60cf35293496d08152e5a3945b76cf1d70cf
Norbert Bartels <n.bartels@phpmonkeys.de>
2022-05-23 22:07:42 +0200

    Issue #1213 - rtbdynamicpost added

---
+++ src/main/lombok/com/restfb/types/ads/RTBDynamicPost.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2010-2022 Mark Allen, Norbert Bartels.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.restfb.types.ads;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.restfb.Facebook;
+import com.restfb.types.Comments;
+import com.restfb.types.FacebookType;
+import com.restfb.types.Likes;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class RTBDynamicPost extends FacebookType {
+
+  @Getter
+  @Setter
+  @Facebook("child_attachments")
+  private List<DynamicPostChildAttachment> childAttachments = new ArrayList<>();
+
+  @Getter
+  @Setter
+  @Facebook
+  private Date created;
+
+  @Getter
+  @Setter
+  @Facebook
+  private String description;
+
+  @Getter
+  @Setter
+  @Facebook("image_url")
+  private String imageUrl;
+
+  @Getter
+  @Setter
+  @Facebook
+  private String link;
+
+  @Getter
+  @Setter
+  @Facebook
+  private String message;
+
+  @Getter
+  @Setter
+  @Facebook("owner_id")
+  private String ownerId;
+
+  @Getter
+  @Setter
+  @Facebook("place_id")
+  private String placeId;
+
+  @Getter
+  @Setter
+  @Facebook("product_id")
+  private String productId;
+
+  @Getter
+  @Setter
+  @Facebook
+  private String title;
+
+  @Getter
+  @Setter
+  @Facebook
+  private Comments comments;
+
+  @Getter
+  @Setter
+  @Facebook
+  private Likes likes;
+
+}
---
+++ src/test/java/com/restfb/types/ads/RTBDynamicPostTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2010-2022 Mark Allen, Norbert Bartels.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.restfb.types.ads;
+
+import com.restfb.AbstractJsonMapperTests;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RTBDynamicPostTest extends AbstractJsonMapperTests {
+
+  @Test
+  void simpleCheck() {
+    RTBDynamicPost dynamicPost =
+        createJsonMapper().toJavaObject(jsonFromClasspath("ads/v9_0/RTBDynamicPost_simple"), RTBDynamicPost.class);
+    checkRTBDynamicPostObject(dynamicPost);
+  }
+
+  @Test
+  void childCheck() {
+    RTBDynamicPost dynamicPost =
+        createJsonMapper().toJavaObject(jsonFromClasspath("ads/v9_0/RTBDynamicPost_childs"), RTBDynamicPost.class);
+    checkRTBDynamicPostObject(dynamicPost);
+    assertNotNull(dynamicPost.getChildAttachments());
+    assertEquals(3L, dynamicPost.getChildAttachments().size());
+
+    DynamicPostChildAttachment attachment = dynamicPost.getChildAttachments().get(0);
+    assertEquals(" ", attachment.getDescription());
+    assertEquals("https://cdn.greatcloudprovider.org/images/aa8fba353b951d9a3af41e819b1cd86b",
+      attachment.getImageUrl());
+    assertEquals("https://www.ourwebshop.com/p/42364", attachment.getLink());
+    assertEquals("898989898989898", attachment.getProductId());
+    assertEquals("great microphone", attachment.getTitle());
+  }
+
+  private void checkRTBDynamicPostObject(RTBDynamicPost dynamicPost) {
+    assertNotNull(dynamicPost);
+    assertEquals("9876543212346", dynamicPost.getId());
+    assertEquals(1L, dynamicPost.getComments().getTotalCount());
+    assertEquals(1652629415000L, dynamicPost.getCreated().getTime());
+    assertEquals("save delivery costs", dynamicPost.getMessage());
+    assertEquals("great microphone", dynamicPost.getTitle());
+    assertEquals("https://www.ourwebshop.com/p/42364", dynamicPost.getLink());
+    assertEquals("3636363636363", dynamicPost.getOwnerId());
+    assertNull(dynamicPost.getPlaceId());
+    assertEquals("898989898989898", dynamicPost.getProductId());
+    assertEquals(1L, dynamicPost.getComments().getTotalCount());
+  }
+}
