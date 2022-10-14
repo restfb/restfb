e61030894ee540371b1aa76bb7bf8c8f77e2e35b
Norbert Bartels <n.bartels@phpmonkeys.de>
2022-10-14 10:20:14 +0200

    Issue #1256 - content publishing limit type added

---
+++ src/main/lombok/com/restfb/types/instagram/IgContentPublishingLimit.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2010-2022 Mark Allen, Norbert Bartels.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.restfb.types.instagram;
+
+import com.restfb.Facebook;
+import com.restfb.types.AbstractFacebookType;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * return type of the content
+ * <a href="https://developers.facebook.com/docs/instagram-api/reference/ig-user/content_publishing_limit">publishing
+ * limit endpoint</a>
+ */
+public class IgContentPublishingLimit extends AbstractFacebookType {
+
+  @Getter
+  @Setter
+  @Facebook("quota_usage")
+  private String quotaUsage;
+
+  @Getter
+  @Setter
+  @Facebook
+  private Config config;
+
+  public static class Config {
+
+    @Getter
+    @Setter
+    @Facebook("quota_total")
+    private Long quotaTotal;
+
+    @Getter
+    @Setter
+    @Facebook("quota_duration")
+    private Long quotaDuration;
+  }
+}
