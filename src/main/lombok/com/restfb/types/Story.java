6323ec427ef095bbc6d8aec36b8135e0e05bfe8f
Norbert Bartels <n.bartels@phpmonkeys.de>
2023-11-17 22:30:07 +0100

    feat: Facebook Story type added
    ref: #1357

---
+++ src/main/lombok/com/restfb/types/Story.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2010-2023 Mark Allen, Norbert Bartels.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.restfb.types;
+
+import com.restfb.Facebook;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+/**
+ * Represents the <a href="https://developers.facebook.com/docs/page-stories-api/#get-stories">Story Graph API type</a>.
+ */
+@Getter
+@Setter
+public class Story extends AbstractFacebookType {
+
+  @Facebook("post_id")
+  private String postId;
+
+  @Facebook("status")
+  private String status;
+
+  @Facebook("creation_time")
+  private Date creationTime;
+
+  @Facebook("media_type")
+  private String mediaType;
+
+  @Facebook("media_id")
+  private String mediaId;
+
+  @Facebook("url")
+  private String url;
+
+  public String getPostId() {
+    return this.postId;
+  }
+
+  public String getStatus() {
+    return this.status;
+  }
+
+  public Date getCreationTime() {
+    return this.creationTime;
+  }
+
+  public String getMediaType() {
+    return this.mediaType;
+  }
+
+  public String getMediaId() {
+    return this.mediaId;
+  }
+
+  public String getUrl() {
+    return this.url;
+  }
+}
---
+++ src/test/java/com/restfb/types/StoryTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2010-2023 Mark Allen, Norbert Bartels.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.restfb.types;
+
+import com.restfb.AbstractJsonMapperTests;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class StoryTest extends AbstractJsonMapperTests {
+
+  @Test
+  void checkStory() {
+    Story story = createJsonMapper().toJavaObject(jsonFromClasspath("v18_0/story"), Story.class);
+
+    assertEquals("post_id", story.getPostId());
+    assertEquals("PUBLISHED", story.getStatus());
+    assertEquals(123456000, story.getCreationTime().getTime()); // Hier könnte eine angepasste Logik erforderlich sein,
+                                                             // abhängig vom Datumsformat
+    assertEquals("video", story.getMediaType());
+    assertEquals("video_id", story.getMediaId());
+    assertEquals("https://facebook.com/stories/123456", story.getUrl());
+  }
+}
