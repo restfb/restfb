bd9410a32fcfed15a1c31920e7f81e87a45df1bb
Norbert Bartels <n.bartels@phpmonkeys.de>
2024-12-30 16:31:30 +0100

    build: License info changed
    ref: #1512

--- src/main/lombok/com/restfb/types/whatsapp/platform/send/Interactive.java
+++ src/main/lombok/com/restfb/types/whatsapp/platform/send/Interactive.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2024 Mark Allen, Norbert Bartels.
+ * Copyright (c) 2010-2025 Mark Allen, Norbert Bartels.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
