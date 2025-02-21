fb2704e37d25e882f2d83e28f654ca0422e02135
Norbert Bartels <n.bartels@phpmonkeys.de>
2025-02-21 12:09:33 +0100

    fix: Fix for fix ...

--- src/test/java/com/restfb/AccessTokenTest.java
+++ src/test/java/com/restfb/AccessTokenTest.java
@@ -36,6 +36,5 @@ class AccessTokenTest extends AbstractJsonMapperTests {
     assertNotNull(accessToken.getAccessToken());
     assertEquals("1234567890", accessToken.getUserId());
     assertEquals(4, accessToken.getPermissions().size());
-    assertEquals("instagram_business_basic,instagram_business_manage_messages,instagram_business_manage_comments,instagram_business_content_publish", accessToken.getPermissionsAsString());
   }
 }
